aeca55490d8ce98fdca88d8feb3c53fdd6bf00b6
Vita <vitalia@myrambler.ru>
2020-04-18 12:11:09 +0300

    + basic json displaying, + stylelint config
    stylelint config will calm postcss lint warnings down

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,16 +1,33 @@
 <template>
   <div class="board">
-
+    <div class="flex flex-row items-start">
+      <div class="column" v-for="(column, $colInd) of board.columns" :key="$colInd">
+        <div class="flex items-center mb-2 font-bold">{{column.name}}</div>
+        <div class="list-reset">
+          <div class="task" v-for="(task, $taskInd) of column.tasks" :key="$taskInd">
+            <span class="w-full flex-no-shrink font-bold">{{task.name}}</span>
+            <p
+              v-if="task.description"
+              class="w-full flex-no-shrink mt-1 text-sm"
+            >{{task.description}}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-export default {
 
-}
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  computed: mapState(["board"])
+};
 </script>
 
-<style lang="css">
+<style type="text/postcss">
 .task {
   @apply flex items-center flex-wrap shadow mb-2 py-2 px-2 rounded bg-white text-grey-darkest no-underline;
 }
@@ -26,6 +43,6 @@ export default {
 
 .task-bg {
   @apply pin absolute;
-  background: rgba(0,0,0,0.5);
+  background: rgba(0, 0, 0, 0.5);
 }
 </style>
