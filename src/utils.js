715a387fdbf0b0d28a69697d36c87893a4b8b180
Vita <vitalia@myrambler.ru>
2020-04-18 12:20:10 +0300

    + board saving (localStorage)

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,12 @@
-export function uuid () {
-  return Math.random().toString(16).slice(2)
+export function uuid() {
+  return Math.random()
+    .toString(16)
+    .slice(2);
 }
 
-export function saveStatePlugin (store) {
-  store.subscribe(
-    (mutation, state) => localStorage.setItem('boards', JSON.stringify(state.boards))
-  )
+export function saveStatePlugin(store) {
+  store.subscribe((
+    mutation,
+    state // subscribing to boards changes and executing localstorage function on change
+  ) => localStorage.setItem("board", JSON.stringify(state.board)));
 }
